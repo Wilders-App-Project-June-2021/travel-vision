20842d03b7d9ec8e49e48aa05c6ab95d32439e02
Lorianne Aguilar <lorianne.a.n@gmail.com>
2021-07-08 16:30:39 +0200

    Edits to city fetching algorithm

--- src/components/Cover.js
+++ src/components/Cover.js
@@ -5,40 +5,44 @@ import React, {useEffect, useState} from "react"
 
 const Cover = ()=>{
 
-const [cities,setCities]= useState("")
-
-// It triggers the request on button click 
-const handleCityRequest=()=>{
-    axios.get("https://api.countrystatecity.in/v1/countries/IT/cities", {
-        headers:{
-            'X-CSCAPI-KEY':'U0dLM3pIV21QSUxYbDZQeUtOQjF0OHJOWmV0RWFCbVFkeUZnS3BKdA=='
-        }
-    })
-    
-    .then(result => setCities(result.data))
-    .catch(error => console.log('error', error))
-}
+    const [cities, setCities]= useState("")
 
 
-console.log(cities)
+    // It triggers the request on button click 
+    const handleCityRequest=()=>{
+        axios.get("https://api.countrystatecity.in/v1/countries/IT/cities", {
+            headers:{
+                'X-CSCAPI-KEY':'U0dLM3pIV21QSUxYbDZQeUtOQjF0OHJOWmV0RWFCbVFkeUZnS3BKdA=='
+            }
+        })
+        .then((result) => {
+            consresult.filter((city)=>city.name === "Parma")
+        })
+        .catch(error => console.log('error', error))
+    }
 
-return (
-    <div className="wrapper">
 
-        <img className="logo" src="../img/landscape.png" alt="Logo" />
 
-        <div> 
-        <input type="text" placeholder="Enter your destination"></input>
-            <select>
-            <option>1</option>
-            </select>
-        <button onClick={()=>handleCityRequest()}>GO</button>
+
+
+    return (
+        <div className="wrapper">
+
+            <img className="logo" src="../img/landscape.png" alt="Logo" />
+
+            <div> 
+            <input type="text" placeholder="Enter your destination"></input>
+                <select>
+                <option>1</option>
+                </select>
+            <button onClick={()=>handleCityRequest()}>GO</button>
+            </div>
+
+
         </div>
 
-
-    </div>
-
-)}
+    )
+}
 
 
 export default Cover
