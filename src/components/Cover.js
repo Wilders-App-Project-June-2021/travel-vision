5e3783e1a7669bc18a75e825ebe3f1e967cc3475
Salvo <salvopatti110@gmail.com>
2021-07-09 15:24:32 +0200

    implemented new API to validate the city input from the user

--- src/components/Cover.js
+++ src/components/Cover.js
@@ -1,26 +1,27 @@
-
+//😎 😎 😎 😎 😎 😎 😎 😎 😎
 import "./Styles.css"
 import axios from "axios"
 import React, {useEffect, useState} from "react"
 
 const Cover = ()=>{
 
+// COUNTRY AND CITY CODES 
     const [cities, setCities]= useState("")
 
 
     // It triggers the request on button click 
     const handleCityRequest=()=>{
-        axios.get("https://api.countrystatecity.in/v1/countries/IT/cities", {
-            headers:{
-                'X-CSCAPI-KEY':'U0dLM3pIV21QSUxYbDZQeUtOQjF0OHJOWmV0RWFCbVFkeUZnS3BKdA=='
-            }
-        })
-        .then((result) => {
-            consresult.filter((city)=>city.name === "Parma")
-        })
+        axios.get("https://api.openweathermap.org/data/2.5/weather?q=Rome,IT&appid=3bc542e8c782c67d428b24c156b77cab")
+        .then((result) => 
+            console.log(result.data))
         .catch(error => console.log('error', error))
-    }
+    } 
+    
+    // https://openweathermap.org/api
+    // Your API key is 3bc542e8c782c67d428b24c156b77cab
 
+
+// GREETING IN LANGUAGE CODE
 
 
 
@@ -31,10 +32,10 @@ const Cover = ()=>{
             <img className="logo" src="../img/landscape.png" alt="Logo" />
 
             <div> 
-            <input type="text" placeholder="Enter your destination"></input>
-                <select>
-                <option>1</option>
-                </select>
+            <input type="text" id="country" value="" placeholder="Destination Country"></input>
+            <br /><br />
+            <input type="text" id="city" value="" placeholder="Destination City"></input>
+            <br /><br />
             <button onClick={()=>handleCityRequest()}>GO</button>
             </div>
 
@@ -46,3 +47,4 @@ const Cover = ()=>{
 
 
 export default Cover
+
