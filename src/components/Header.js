2004b2d10ab478e314450e4bc1522d60bd1f17de
Lorianne Aguilar <lorianne.a.n@gmail.com>
2021-07-21 16:08:20 +0200

    Created Input component

--- src/components/Header.js
+++ src/components/Header.js
@@ -17,22 +17,8 @@ const Header = ()=> {
                     </div>
                 </div>
             </div>
-            
-            {/* <div className="input-box-desktop">
-                New destination? &nbsp;
-
-                <form onSubmit={(e)=>getCityInfo(e)}>
-                    <select className="input-country-desktop" id="country" placeholder="Country" required onChange={(e)=>handleCountryInput(e)}>
-                        <option value="">Select a Country</option>
-                        {nations.map((country,i)=> <option id={country.name} key={i} value={country.code}> {country.name} </option>)}
-                    </select>
-                    <input className="input-city-desktop" type="text" id="city" placeholder="Enter a City" required onChange={(e)=> handleCityinput(e)}></input>
-                    {props.error? <h1>Sorry we couldn't find this city, try again</h1>:null}
-                    <button className="go" type="submit">Go</button>
-                </form>
-            </div> */}
+        {/* <Input /> */}
         </div>
-
         )
 
 }
