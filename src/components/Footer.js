eaa7df8b3ead019eab29b151efd4155ec2e02339
Lorianne Aguilar <lorianne.a.n@gmail.com>
2021-07-21 21:37:24 +0200

    Special effetx

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,14 @@
 import "./Footer.css";
 import "./vivify.min.css"
+import github from './github.svg';
+import linkedin from './linkedin.svg';
 import React, {useState, useEffect}from "react";
 
 
 const Footer = ()=> {
 
+    const [lorianne, setLorianne]= useState('./img/lorianne.jpg');
+    const [salvo, setSalvo]= useState('./img/salvatore.png');
 
     return (
 
@@ -13,12 +17,12 @@ const Footer = ()=> {
                 <span className="made-by">Made by <a href="http://wildcodeschool.com" target="new">Wild Code School</a> Students</span>
                 <div className="students">
                     <div className="student-box">
-                        <a href="https://www.linkedin.com/in/lorianne-aguilar/" target="new"><img class="footer-img" src="./img/lorianne.jpg" alt="Lorianne" /></a>
-                        <a href="https://github.com/Grailsidhe" target="new"><p className="names">Lorianne</p></a>
+                        <a href="https://github.com/Grailsidhe" target="new"><img class="footer-img" src={lorianne} onMouseOver={()=>setLorianne(`${github}`)} onMouseOut={() => setLorianne('./img/lorianne.jpg')} alt="Lorianne" /></a>
+                        <a href="https://www.linkedin.com/in/lorianne-aguilar/" target="new"><p className="names">Lorianne</p></a>
                     </div>
                     <div className="student-box">
-                        <a href="https://github.com/sal9110" target="new"><img class="footer-img" src="./img/salvatore.png" alt="Salvatore" /></a>
-                        <a href="https://github.com/sal9110" target="new"><p className="names">Salvatore</p></a>
+                        <a href="https://github.com/sal9110" target="new"><img class="footer-img" src={salvo} onMouseOver={()=>setSalvo(`${github}`)} onMouseOut={() => setSalvo('./img/salvatore.png')} alt="Salvatore" /></a>
+                        <a href="http://linkedin.com/in/salvatore-patti-9b5198141 " target="new"><p className="names">Salvatore</p></a>
                     </div>
                     <div className="student-box">
                         <a href="" target="new"><img class="footer-img" src="" alt="" /></a>
