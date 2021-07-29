73e79cf7e5d20a1320f905c5e31c63060a6bcf01
Lorianne Aguilar <lorianne.a.n@gmail.com>
2021-07-29 18:37:39 +0200

    Added surnames to footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -19,15 +19,15 @@ const Footer = ()=> {
                 <div className="students">
                     <div className="student-box">
                         <a href="https://github.com/Natalito96" target="new"><img className="footer-img" src={natalie} onMouseOver={()=>setNatalie(`${github}`)} onMouseOut={() => setNatalie('./img/natalie.jpg')} alt="Natalie" /></a>
-                        <a href="https://www.linkedin.com/in/natalie-huaman-730bb4213/" target="new" className="a"><p className="names">Natalie</p></a>
+                        <p className="names"><a href="https://www.linkedin.com/in/natalie-huaman-730bb4213/" target="new" className="a">Natalie</a><br/><span className="surnames">Huaman</span></p>
                     </div>
                     <div className="student-box">
                         <a href="https://github.com/Grailsidhe" target="new"><img className="footer-img" src={lorianne} onMouseOver={()=>setLorianne(`${github}`)} onMouseOut={() => setLorianne('./img/lorianne.jpg')} alt="Lorianne" /></a>
-                        <a href="https://www.linkedin.com/in/lorianne-aguilar/" target="new" className="a"><p className="names">Lorianne</p></a>
+                        <p className="names"><a href="https://www.linkedin.com/in/lorianne-aguilar/" target="new" className="a">Lorianne</a><br /><span className="surnames">Aguilar</span></p>
                     </div>
                     <div className="student-box">
                         <a href="https://github.com/sal9110" target="new"><img className="footer-img" src={salvo} onMouseOver={()=>setSalvo(`${github}`)} onMouseOut={() => setSalvo('./img/salvatore.png')} alt="Salvatore" /></a>
-                        <a href="http://linkedin.com/in/salvatore-patti-9b5198141" target="new" className="a"><p className="names">Salvatore</p></a>
+                        <p className="names"><a href="http://linkedin.com/in/salvatore-patti-9b5198141" target="new" className="a">Salvatore</a><br /><span className="surnames">Patti</span></p>
                     </div>
                 </div>
                 <span className="small">@ 2021 Travel Vision</span>
