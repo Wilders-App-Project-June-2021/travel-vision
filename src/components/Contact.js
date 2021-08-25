f9edb083a070edf9c7b03ea7686d5375be0b1d52
Lorianne Aguilar <lorianne.a.n@gmail.com>
2021-08-25 18:44:17 +0200

    Added email input

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -13,11 +13,13 @@ const Contact = ()=> {
             <div className="message-fields">
             <div className="contact-name-desktop">
                 <input type="text" id="fname" name="firstname" placeholder="First name" className="contact-input-desktop" />&nbsp;
-                <input type="text" id="lname" name="lastname" placeholder="Last name" className="contact-input-desktop" />
+                <input type="text" id="lname" name="lastname" placeholder="Last name" className="contact-input-desktop" />&nbsp;
+                <input type="text" id="email" name="email" placeholder="email@emailaddress.com" className="contact-input-desktop" />
             </div>
             <div className="contact-name-mobile">
                 <input type="text" id="fname" name="firstname" placeholder="First name" className="contact-input" />
                 <input type="text" id="lname" name="lastname" placeholder="Last name" className="contact-input" />
+                <input type="text" id="email" name="email" placeholder="email@emailaddress.com" className="contact-input" />
             </div>
                 <input type="text" id="title" name="title" placeholder="Title" className="contact-input" />
                 <textarea id="subject" name="subject" placeholder="Message" style={{height: "200px"}} className="contact-input"></textarea>
