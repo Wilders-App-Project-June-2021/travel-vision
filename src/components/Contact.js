4649976ac5e54f4f615c65fdfd44a41f612688b6
Lorianne Aguilar <lorianne.a.n@gmail.com>
2021-08-25 17:04:27 +0200

    Added contact form

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,4 +1,5 @@
-import "./vivify.min.css"
+import "./vivify.min.css";
+import "./Main.css";
 import React, {useState, useEffect}from "react";
 
 
@@ -6,12 +7,17 @@ const Contact = ()=> {
 
 
     return (
-        <div className="fadeIn">
-            
-
-
-
-            
+        <div className="contact-form-wrapper fadeIn">
+            Fill out the form and we'll get back to you shortly.
+            <form>
+            <div className="message-fields">
+                <input type="text" id="fname" name="firstname" placeholder="First name" className="contact-input" />
+                <input type="text" id="lname" name="lastname" placeholder="Last name" className="contact-input" />
+                <input type="text" id="title" name="title" placeholder="Title" className="contact-input" />
+                <textarea id="subject" name="subject" placeholder="Message" style={{height: "200px"}} className="contact-input"></textarea>
+                <input type="submit" value="Submit" className="contact-input contact-form-button" />
+            </div>
+            </form>
         </div>
     )
 }
